1a04add5c4cb84e6a8e55964bc488aba8f336352
Kot Dymok <kot.dymok+git@gmail.com>
2019-01-13 03:45:59 +0300

    After 1h 20m

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,80 @@ module Main where
 
 import Lib
 
+import Control.Monad
+import qualified Graphics.UI.Threepenny as UI
+import Graphics.UI.Threepenny.Core
+
 main :: IO ()
-main = someFunc
+main = do
+  startGUI defaultConfig setup
+
+setup :: Window -> UI ()
+setup window = void $ do
+  return window # set title "Game of Live"
+  let
+    defaultHeight = 550
+    defaultWidth  = defaultHeight * 2
+    scaleFactor = 10
+    defaultLengthX = defaultWidth `div` scaleFactor
+    defaultLengthY = defaultHeight `div` scaleFactor
+    defaultTimerStep = ceiling $ 0.1 * 1000 -- in ms
+    defaultGrid = replicate defaultLengthX $ replicate defaultLengthY False 
+    defaultGame = Game defaultGrid [3] [2, 3]
+
+  canvas       <- UI.canvas
+                  # set UI.width  defaultWidth
+                  # set UI.height defaultHeight
+                  # set style [("border", "solid black 1px"), ("background", "#fff")]
+  timer        <- UI.timer # set UI.interval defaultTimerStep
+  activate     <- UI.button #+ [string "Run/Pause"]
+  nX           <- UI.input  # set value (show defaultLengthX)
+  nY           <- UI.input  # set value (show defaultLengthY)
+  changeX      <- UI.input  # set value (show 0)
+  changeY      <- UI.input  # set value (show 0)
+  changeValue  <- UI.input  # set value (show False)
+  changeButton <- UI.button #+ [string "Run/Pause"]
+  clearCanvas  <- UI.button #+ [string "Clear the canvas."]
+  clearGrid    <- UI.button #+ [string "Clear the grid."]
+
+  getBody window #+
+    [ row [
+        column [ element clearCanvas
+               , element activate
+               ],
+        column [ row [string "Rows:" # set style [("width","60px")], element nY]
+               , row [string "Columns:" # set style [("width","60px")], element nX]
+               ],
+        column [ row [string "X" # set style [("width","60px")], element changeX]
+               , row [string "Y" # set style [("width","60px")], element changeY]
+               ],
+        column [ row [string "Value" # set style [("width","60px")], element changeValue]
+               , element changeButton
+               ]
+        ],
+        element canvas
+    ]
+  UI.start timer
+
+  nXValue <- stepper "" $ UI.valueChange nX
+  nYValue <- stepper "" $ UI.valueChange nY
+  changeXValue <- stepper "" $ UI.valueChange changeX
+  changeYValue <- stepper "" $ UI.valueChange changeY
+  changeVValue <- stepper "" $ UI.valueChange changeValue
+  mousePosition <- stepper (0, 0) $ UI.mousemove canvas
+
+  isActive <- accumB False (not <$ UI.click activate)
+  let
+    nXYValues = liftA2 (\x y -> (x, y)) nXValue nYValue
+    clearBehaviour :: UI.Event (Game -> Game)
+    clearBehaviour = (\(nx, ny) g -> g {Lib.grid = replicate (read nx) $ replicate (read ny) False}) <$> nXYValues <@ (UI.click clearGrid)
+
+    unionWith' :: [UI.Event a] -> UI.Event a
+    unionWith' [] = undefined
+    unionWith' (e1:es) | length es == 1 = UI.unionWith const e1 $ head es
+                       | otherwise      = UI.unionWith const e1 $ unionWith' es
+    summaryChanges = unionWith' [clearBehaviour]
+  worldEvent <- accumE defaultGame summaryChanges
+  
+  on UI.click clearCanvas $ const $
+    canvas # UI.clearCanvas
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,77 @@
 module Lib
-    ( someFunc
+    ( Game (..),
+      parseRules,
+      step,
+      getValue,
+      setValue
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List.Split
+
+data Game = Game {
+  grid :: [[Bool]],
+  born :: [Int],
+  survives :: [Int]
+  }
+  deriving Show
+
+
+checkBounds :: Int -> Int -> Int
+checkBounds x lx | x > lx = x - lx
+                 | x < 0 = lx + x
+                 | otherwise = x
+
+
+getValue :: Game -> Int -> Int -> Bool
+getValue (Game g _ _) x y = g !! x' !! y' 
+  where
+    lx = length g
+    ly = length . head $ g
+    x' = checkBounds x lx
+    y' = checkBounds y ly
+                
+setValue :: Game -> Int -> Int -> Bool -> Game
+setValue g x y v = g {grid = g'}
+  where
+    lx = length $ grid g
+    ly = length . head $ grid g
+    x' = checkBounds x lx
+    y' = checkBounds y ly
+    (beforeX, currX:afterX) = splitAt x' $ grid g
+    (beforeY, _:afterY) = splitAt y' currX
+    g' = beforeX ++ [beforeY++[v]++afterY] ++ afterX
+
+nLiveNeighbors :: Game -> Int -> Int -> Int
+nLiveNeighbors g x y = length $ filter id neighs
+  where
+    neighs = [getValue g (x+x') (y+y') | x' <- [-1..1] , y' <- [-1..1], x' /= 0 || y' /= 0]
+
+nextValue :: Game -> Bool -> Int -> Bool
+nextValue (Game g b s) v n | v == False && elem n b = True
+                           | v == True  && elem n s = True
+                           | otherwise              = False
+
+step :: Game -> Game
+step g = g {grid = grid'}
+  where
+    lx = length $ grid g
+    ly = length . head $ grid g
+    nv :: Int -> Int -> Bool
+    nv x' y' = nextValue g v n
+      where
+        v = getValue g x' y'
+        n = nLiveNeighbors g x' y'
+    grid' = [[nv x y | y <- [0 .. ly - 1]] | x <- [0 .. lx-1]]
+
+parseRules :: String -> ([Int], [Int])
+parseRules string = foldr (\(x,y) (x',y') -> (x++x',y++y')) ([],[]) rules
+  where
+    substrings = splitOn "/" string
+    oneRule :: String -> ([Int], [Int])
+    oneRule "" = ([],[])
+    oneRule (s:ss) | s == 'B'  = (converted, [])
+                   | s == 'S'  = ([], converted)
+                   | otherwise = ([],[])
+      where
+        converted = map read $ chunksOf 1 ss :: [Int]
+    rules = map oneRule substrings
